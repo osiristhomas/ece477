9f725b867384d277fc561fe319b6eeffe0529fc3
Osiris Thomas <osiris.thomas@maine.edu>
2021-02-19 16:31:05 -0500

    fixed direction function. Added debug statement to direction function.

--- lab2/led.c
+++ lab2/led.c
@@ -57,7 +57,8 @@ void direction(const char *pin, char *dir)
         strcat(path, "/direction");
         int file = open(path, O_WRONLY);
         // write the value passed into the file
-        write(file, dir, 2);
+        write(file, dir, sizeof(char) * 5);
+	printf("Wrote \"out\" to %s\n", path);
         close(file);
         free(path);
 }
--- lab2/main.c
+++ lab2/main.c
@@ -12,33 +12,37 @@
 
 int main(void)
 {
+	
 	int i;
 	char pins[8][3] = {"18", "23", "24", "25", "12", "16", "20", "21"};
 	char pin[3];
 	// export all used pins
 	for (i = 0; i < 8; i++) {
-		//snprintf(pin, 2, "%d", i);
 		strcpy(pin, pins[i]);
 		printf("Initializing GPIO %s\n", pin);
 		exportpin(pin);
+		usleep(1000);
 		direction(pin, "out");
-		sleep(1);
+		usleep(1000);
 	}
 	sleep(1);
 	for (i = 0; i < 8; i++) {
-		//snprintf(pin, 2, "%d", i);
 		strcpy(pin, pins[i]);
 		value(pin, "1");
-		sleep(1);
+		usleep(1000);
 	}
-
+	sleep(1);
+	for (i = 0; i < 8; i++) {
+		strcpy(pin, pins[i]);
+		value(pin, "0");
+		usleep(1000);
+	}
 	// unexport all used pins	
 	for (i = 0; i < 8; i++) {
-		//snprintf(pin, 2, "%d", i);
 		strcpy(pin, pins[i]);
 		printf("Unexporting GPIO %s\n", pin);
 		unexport(pin);
-		sleep(1);
+		usleep(1000);
 	}
 	return 0; 
 }
