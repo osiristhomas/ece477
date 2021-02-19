bef184a302aa99b64a248ee8971d41066d166b73
Osiris Thomas <osiris.thomas@maine.edu>
2021-02-19 15:14:53 -0500

    initial commit with code that initializes all GPIO pins used

---
+++ lab2/led.c
@@ -0,0 +1,63 @@
+/* Osiris Thomas
+ * led.c
+ * Functions for handling GPIO
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "led.h"
+// exports pin passed
+void exportpin(const char *pin)
+{
+	int file = open("/sys/class/gpio/export", O_WRONLY);
+	if (file == -1) 
+		printf("Error in export, open\n");
+	// export pin
+	if (write(file, pin, 2) > 2)
+		printf("Error in export, write\n");
+	close(file);
+}
+
+// unexports pin passed
+void unexport(const char *pin)
+{
+	int file = open("/sys/class/gpio/unexport", O_WRONLY);
+	// unexport pin
+	write(file, pin, 2);
+	close(file);
+}
+
+// writes passed value to passed pin
+void value(const char *pin, char *value)
+{
+	char *path = malloc(FILENAME_MAX);
+	// create string of pin file
+	strcpy(path, "/sys/class/gpio/gpio");
+	strncat(path, pin, 2);
+	strncat(path, "/value", 10);
+	int file = open(path, O_WRONLY);
+	if (file == -1)
+		printf("Cannot open %s\n", path);
+	printf("Writing %s to %s\n", value, path);
+	write(file, value, 2);
+	close(file);
+	free(path);
+}
+
+// sets the direction
+void direction(const char *pin, char *dir)
+{
+	char *path = malloc(FILENAME_MAX);
+        // create string of pin file
+        strcpy(path, "/sys/class/gpio/gpio");
+        strcat(path, pin);
+        strcat(path, "/direction");
+        int file = open(path, O_WRONLY);
+        // write the value passed into the file
+        write(file, dir, 2);
+        close(file);
+        free(path);
+}
---
+++ lab2/led.h
@@ -0,0 +1,12 @@
+/* Osiris Thomas
+ * led.h
+ * required function prototypes for led.c
+ */
+
+void exportpin(const char *);
+
+void unexport(const char *);
+
+void value(const char *, char *);
+
+void direction(const char *, char *);
