e1dbd20705e9bc0211cd5d6137c3f3e10a4a36ea
osiris.thomas <osiris.thomas@maine.edu>
2021-03-08 14:16:35 -0500

    Added files for lab 4

---
+++ lab4/cool.c
@@ -0,0 +1,78 @@
+/* Osiris Thomas
+ * ECE 477 Lab 4
+ * cool.c
+ * Pong-esque game using GPIO
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <wiringPi.h>
+
+#define A digitalRead(0)
+#define B digitalRead(1)
+#define FW 0
+#define BW 1
+
+// Configure gpio to match hardware.
+void init_gpio(void);
+// Cycle through LED array depending on speed and direction
+void cycle_leds(int, int);
+
+int main(int argc, char *argv[])
+{
+	// Initial time delay is half a second
+	int del = 512 * 1000;
+	int level = 1;
+	wiringPiSetup();
+	init_gpio();
+
+	while (1) {
+		// "Serve" the ball
+		cycle_leds(del, BW);
+		if (A) {
+			// Return the ball if A is pressed
+			cycle_leds(del, FW);
+		} else {
+			printf("Made it to level %d!\n", level);
+			return 0;	
+		}
+		// Incrememt level and make the time delay shorter by 50%
+		level++;
+		del /= 1.5;
+	}
+}
+
+// Configure gpio to match hardware.
+void init_gpio(void)
+{
+	/* Pins 0 and 1 are inputs and are pulled up to 3.3V.
+	 * Pins 2-7 are outputs tied to LEDs.
+	 */
+	int i;
+	for (i = 0; i < 2; i++) {
+		pinMode(i, INPUT);
+		pullUpDnControl(i, PUD_UP);
+	}
+	for (i = 2; i < 8; i++) {
+		pinMode(i, OUTPUT);
+	}
+}
+
+// Cycle LEDs 
+void cycle_leds(int delay, int dir)
+{
+	int i;
+	if (dir == FW) {
+		for (i = 2; i < 7; i++) {
+			digitalWrite(i, HIGH);
+			usleep(delay);
+			digitalWrite(i, LOW);	
+		}	
+	} else {
+		for (i = 7; i > 2; i--) {
+			digitalWrite(i, HIGH);
+			usleep(delay);
+			digitalWrite(i, LOW);
+		}
+	}
+}
---
+++ lab4/main.c
@@ -0,0 +1,75 @@
+/* Osiris Thomas
+ * ECE 477 Lab 4
+ * main.c
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <wiringPi.h>
+
+#define A digitalRead(0)
+#define B digitalRead(1)
+#define FW 0
+#define BW 1
+
+// Configure gpio to match hardware.
+void init_gpio(void);
+// Cycle through LED array depending on speed and direction
+void cycle_leds(int, int);
+
+int main(int argc, char *argv[])
+{
+	int del = 1024 * 1000;
+	int dir = FW;
+	wiringPiSetup();
+	init_gpio();
+	// go through loop while both buttons aren't pressed
+	while (1) {
+		if (A && B)
+			return 0;
+		else if (A && (del > 32 * 1000))
+			del /= 2;
+		else if (A && (del == 32 * 1000))
+			dir = BW;
+		else if (B && (del < 1024 * 1000))
+			del *= 2;
+		else if (B && (del == 1024 * 1000))
+			dir = FW;
+		cycle_leds(del, dir);
+	}
+}
+
+// Configure gpio to match hardware.
+void init_gpio(void)
+{
+	/* Pins 0 and 1 are inputs and are pulled down.
+	 * Pins 2-7 are outputs.
+	 */
+	int i;
+	for (i = 0; i < 2; i++) {
+		pinMode(i, INPUT);
+		pullUpDnControl(i, PUD_UP);
+	}
+	for (i = 2; i < 8; i++) {
+		pinMode(i, OUTPUT);
+	}
+}
+
+// Cycle LEDs 
+void cycle_leds(int delay, int dir)
+{
+	int i;
+	if (dir == FW) {
+		for (i = 2; i < 8; i++) {
+			digitalWrite(i, HIGH);
+			usleep(delay);
+			digitalWrite(i, LOW);	
+		}	
+	} else {
+		for (i = 7; i > 1; i--) {
+			digitalWrite(i, HIGH);
+			usleep(delay);
+			digitalWrite(i, LOW);
+		}
+	}
+}
